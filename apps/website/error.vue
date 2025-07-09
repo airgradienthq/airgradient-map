58691529653d0bfc6cf5bd8fa718c001b12f34ba
smjsmsam <smjsmsam@gmail.com>
2025-07-09 08:55:55 -0700

    non-404 error handling

--- apps/website/error.vue
+++ apps/website/error.vue
@@ -1,15 +1,24 @@
-<script setup>
-  useHead({
-    title: "404 Page not found"
+<script setup lang="ts">
+  import type { NuxtError } from 'nuxt/app'
+
+  const props = defineProps({
+    error: Object as () => NuxtError
   })
+
+  if(props.error?.statusCode === 404) {
+    document.title = "404 Page not found"
+  }
+  else {
+    document.title = "Something went wrong"
+  }
+
 </script>
 
 <template>
   <NuxtLayout>
-    <!-- original -->
     <div class="container">
-      <div class="row">
-        <div class="col-md-6 text-center mb-5 mb-md-0">
+      <div class="row justify-center">
+        <div v-if="error?.statusCode === 404" class="col-md-6 text-center mb-5 mb-md-0">
           <img 
             src="https://www.airgradient.com/images/404.jpg" 
             class="img-fluid"
@@ -25,45 +34,5 @@
         </div>
       </div>
     </div>
-
-    <!-- centered + img-focused -->
-    <!-- <div class="vh-100 d-flex align-items-center justify-content-center text-center">
-      <div>
-        <h1 class="display-2 font-weight-bold mb-0">
-          You ran out of air.
-        </h1>
-        <div>
-          <img
-            src="https://www.airgradient.com/images/404.jpg"
-            class="img-fluid"
-            style="max-width: 400px; height: auto;"
-          />
-        </div>
-        <p class="fs-2">We can help with that.</p>
-        <p>But first, let's get out of this page.</p>
-        <NuxtLink to="/" class="btn-large button-blue">
-          Go to Homepage
-        </NuxtLink>
-      </div>
-    </div> -->
-
-    <!-- staggered -->
-    <!-- <div class="container justify-content-center">
-      <div class="row">
-        <h1 class="col-sm text-start text-md-end mt-10">You ran out of air.</h1>
-        <img
-          src="https://www.airgradient.com/images/404.jpg"
-          class="col-sm img-responsive"
-        />
-        <div class="col-sm"></div>
-      </div>
-      <div class="text-end justify-content-center">
-        <p class="">The page you're looking for is beyond our reach...</p>
-        <NuxtLink to="/" class="btn-large button-blue">
-          Go to Homepage
-        </NuxtLink>
-      </div>
-    </div> -->
-
   </NuxtLayout>
 </template>
