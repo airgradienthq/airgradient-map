d1a4e6d02b9105cef0778669e9b15f13b04b74a1
smjsmsam <smjsmsam@gmail.com>
2025-07-09 09:09:32 -0700

    formatting

--- apps/website/error.vue
+++ apps/website/error.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-  import type { NuxtError } from 'nuxt/app'
+  import type { NuxtError } from 'nuxt/app';
 
   const props = defineProps({
     error: Object as () => NuxtError
-  })
+  });
 
-  if(props.error?.statusCode === 404) {
-    document.title = "404 Page not found"
+  if (props.error?.statusCode === 404) {
+    document.title = '404 Page not found';
+  } else {
+    document.title = 'Something went wrong';
   }
-  else {
-    document.title = "Something went wrong"
-  }
-
 </script>
 
 <template>
@@ -19,18 +17,12 @@
     <div class="container">
       <div class="row justify-center">
         <div v-if="error?.statusCode === 404" class="col-md-6 text-center mb-5 mb-md-0">
-          <img 
-            src="./assets/images/404.jpg" 
-            class="img-fluid"
-            alt="404 Error"
-          />
+          <img src="./assets/images/404.jpg" class="img-fluid" alt="404 Error" />
         </div>
         <div class="col-md-6 align-self-center text-center text-md-start">
           <h1 class="mb-4">You ran out of air.</h1>
           <p class="mb-4">The page you're looking for is beyond our reach…</p>
-          <NuxtLink to="/" class="btn-large button-blue">
-            Go to Homepage
-          </NuxtLink>
+          <NuxtLink to="/" class="btn-large button-blue"> Go to Homepage </NuxtLink>
         </div>
       </div>
     </div>
