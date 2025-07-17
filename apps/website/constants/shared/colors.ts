e47cb25cd5a744e62d1a3e94ce9e8012bd8c31ea
Herbil Ioan <ionutherbil@gmail.com>
2025-07-17 14:42:13 +0300

    Fixed the colors for consistency

--- apps/website/constants/shared/colors.ts
+++ apps/website/constants/shared/colors.ts
@@ -5,11 +5,11 @@
 import { ChartColorsType } from '~/types/shared/colors';
 
 export const CHART_COLORS_CSS_VARS: Record<ChartColorsType, string> = {
-  [ChartColorsType.GREEN]: '#1de208',
-  [ChartColorsType.YELLOW]: '#e2e020',
-  [ChartColorsType.ORANGE]: '#e26a05',
-  [ChartColorsType.RED]: '#e20410',
-  [ChartColorsType.PURPLE]: '#7f01e2',
+  [ChartColorsType.GREEN]: '#1DE206',
+  [ChartColorsType.YELLOW]: '#E3E021',
+  [ChartColorsType.ORANGE]: '#FF6701',
+  [ChartColorsType.RED]: '#E20411',
+  [ChartColorsType.PURPLE]: '#7F01E1',
   [ChartColorsType.BROWN]: '#903305',
   [ChartColorsType.BLUE]: '#1b75bc',
   [ChartColorsType.GRAY]: '#778899',
@@ -23,9 +23,9 @@ export const CHART_COLORS_DARKENED_CSS_VARS: Record<ChartColorsType, string> = {
   [ChartColorsType.ORANGE]: '#b94f04',
   [ChartColorsType.RED]: '#881218',
   [ChartColorsType.PURPLE]: '#521681',
-  [ChartColorsType.BROWN]: '#903305',
-  [ChartColorsType.BLUE]: '#02579a',
-  [ChartColorsType.GRAY]: '#7c7c7c',
+  [ChartColorsType.BROWN]: '#54230b',
+  [ChartColorsType.BLUE]: '#134f7e',
+  [ChartColorsType.GRAY]: '#4c5660',
   [ChartColorsType.LIGHTGRAY]: '#989696',
   [ChartColorsType.DEFAULT]: '#989696'
 };
