1e66cffa55111a117a43dde3327c516ba8ec5c60
Mahlet333 <132096863+Mahlet333@users.noreply.github.com>
2025-07-21 21:50:33 +0400

    chore: update areaQuery.ts to match original code

--- apps/api/src/measurement/areaQuery.ts
+++ apps/api/src/measurement/areaQuery.ts
@@ -1,45 +1,33 @@
-import { IsOptional, IsNotEmpty, IsNumber, Min, Max } from 'class-validator';
+import { IsOptional, IsNumber, Min, Max } from 'class-validator';
 import { Type } from 'class-transformer';
-import { ApiProperty } from '@nestjs/swagger';
 
-class AreaQuery {
-  @ApiProperty({ description: 'south latitude of the area' })
-  @IsNotEmpty()
+export default class AreaQuery {
   @IsNumber()
-  @Min(-90)
-  @Max(90)
+  @Min(-180)
+  @Max(180)
   @Type(() => Number)
   xmin: number;
 
-  @ApiProperty({ description: 'west longitude of the area' })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(-180)
-  @Max(180)
-  @Type(() => Number)
-  ymin: number;
-
-  @ApiProperty({ description: 'north latitude of the area' })
-  @IsNotEmpty()
   @IsNumber()
   @Min(-90)
   @Max(90)
   @Type(() => Number)
-  xmax: number;
+  ymin: number;
 
-  @ApiProperty({ description: 'east longitude of the area' })
-  @IsNotEmpty()
   @IsNumber()
   @Min(-180)
   @Max(180)
   @Type(() => Number)
+  xmax: number;
+
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  @Type(() => Number)
   ymax: number;
 
-  @ApiProperty({ description: 'zoom level' })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
-  zoom: number;
+  zoom?: number;
 }
-
-export default AreaQuery;
