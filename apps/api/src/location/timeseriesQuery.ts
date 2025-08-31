3992cf939ae1c4c2bdda61acd0fe1c581f917a10
samuelbles07 <samuelbles07@gmail.com>
2025-09-01 02:58:23 +0700

    Add exception check

--- apps/api/src/location/timeseriesQuery.ts
+++ apps/api/src/location/timeseriesQuery.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 class TimeseriesQuery {
+  // TODO format with timezone
   @ApiProperty({
     default: '2025-02-01 00:00',
     description: 'Start date in format "YYYY-MM-DD" or "YYYY-MM-DD HH:MM"',
@@ -9,6 +10,7 @@ class TimeseriesQuery {
   @IsString()
   start: string;
 
+  // TODO format with timezone and explain if timezone not included, might inconsistent
   @ApiProperty({
     default: '2025-02-07 00:00',
     description: 'End date in format "YYYY-MM-DD" or "YYYY-MM-DD HH:MM"',
@@ -16,6 +18,7 @@ class TimeseriesQuery {
   @IsString()
   end: string;
 
+  // TODO: follow format of bucketSize enum and validate
   @ApiProperty({
     default: '1 D',
     description: 'Bucket size in ISO 8601 duration format (e.g., "5 M", "1 H", "1 D")',
