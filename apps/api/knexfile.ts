e4aa42205d0d8acb830b001296f2f977db9a3fca
samuelbles07 <samuelbles07@gmail.com>
2025-09-19 00:52:28 +0700

    Initialize knex to handle migrations

---
+++ apps/api/knexfile.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex';
+
+const config: Knex.Config = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DATABASE_HOST || 'postgrex',
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    database: 'agmap',
+    user: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'schema_migrations',
+    extension: 'ts'
+  },
+};
+
+export default config;
