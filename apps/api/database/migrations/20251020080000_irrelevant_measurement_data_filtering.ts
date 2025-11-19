ccdbeb7ce92decdf58abdcde78d7b6a8dbd39027
uun <uun@uun.local>
2025-11-19 10:01:21 +0700

    fix(migration): add 'IF NOT EXISTS' to 20251020080000_irrelevant_measurement_data_filtering.ts

--- apps/api/database/migrations/20251020080000_irrelevant_measurement_data_filtering.ts
+++ apps/api/database/migrations/20251020080000_irrelevant_measurement_data_filtering.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   // Add is_pm25_outlier column in public.measurement table
-  await knex.raw('ALTER TABLE public.measurement ADD COLUMN is_pm25_outlier boolean NOT NULL DEFAULT false');
+  await knex.raw('ALTER TABLE public.measurement ADD COLUMN IF NOT EXISTS is_pm25_outlier boolean NOT NULL DEFAULT false;');
 }
 
 export async function down(knex: Knex): Promise<void> {
